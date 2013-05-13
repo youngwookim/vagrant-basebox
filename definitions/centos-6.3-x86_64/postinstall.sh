83ad7c8d81aae65fcd7f3557323a4b688ec64e8f
YoungwooKim <brandon.kim@nexr.com>
2013-05-13 16:48:21 +0900

    Update CentOS 6.3 definitions

--- definitions/centos-6.3-x86_64/postinstall.sh
+++ definitions/centos-6.3-x86_64/postinstall.sh
@@ -2,7 +2,9 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-cat > /etc/yum.repos.d/epel.repo << EOM
+# YUM Repositories
+# EPEL
+cat > /etc/yum.repos.d/epel.repo <<EOM
 [epel]
 name=epel
 baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
@@ -10,17 +12,12 @@ enabled=1
 gpgcheck=0
 EOM
 
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget sudo rsync ntp ntpdate python-setuptools parted bzip2 unzip yum-utils sudo createrepo httpd
 
-# Vagrant specific
-date > /etc/vagrant_box_build_time
-
-# Add vagrant user
-/usr/sbin/groupadd vagrant
-/usr/sbin/useradd vagrant -g vagrant -G wheel
-echo "vagrant"|passwd --stdin vagrant
-echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
-chmod 0440 /etc/sudoers.d/vagrant
+# Services
+/sbin/chkconfig --level 2345 iptables off
+/sbin/chkconfig --level 2345 ip6tables off
+/sbin/chkconfig --level 2345 httpd off
 
 # Installing vagrant keys
 mkdir -pm 700 /home/vagrant/.ssh
@@ -29,17 +26,15 @@ chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 # Installing the virtualbox guest additions
-VBOX_VERSION=$(cat /home/veewee/.vbox_version)
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
-mount -o loop /home/veewee/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -rf /home/veewee/VBoxGuestAdditions_*.iso
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
 
 # Cleanup
-yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y clean all
-rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
 rm -rf VBoxGuestAdditions_*.iso
 
 # Zero out the free space to save space in the final image:
