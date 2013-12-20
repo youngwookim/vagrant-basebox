0dceec3d941f333bf966dfcce9f208f9a31e0449
Youngwoo Kim <brandon.kim@nexr.com>
2013-12-20 14:40:32 +0900

    Disk_size = 20GB

--- definitions/centos-6.4-x86_64-x/definition.rb
+++ definitions/centos-6.4-x86_64-x/definition.rb
@@ -1,7 +1,7 @@
 Veewee::Session.declare({
   :cpu_count => '1',
   :memory_size=> '2048',
-  :disk_size => '10140',
+  :disk_size => '20000',
   :disk_format => 'VDI',
   :hostiocache => 'off', 
   :ioapic => 'on',
